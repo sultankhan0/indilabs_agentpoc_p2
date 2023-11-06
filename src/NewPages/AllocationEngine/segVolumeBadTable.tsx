fb11d47e07d92c73a44ddf2f22ebb7f805b0ae8b
Atherusman <atherusman@mobilefirstapplication.com>
2023-11-06 12:17:39 +0530

    changes in tabs

--- src/NewPages/AllocationEngine/segVolumeBadTable.tsx
+++ src/NewPages/AllocationEngine/segVolumeBadTable.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-// import Sidebar from "../../components/common/Sidebar";
+
 
 
 const SegVolumeBadTable: React.FC = () => {
