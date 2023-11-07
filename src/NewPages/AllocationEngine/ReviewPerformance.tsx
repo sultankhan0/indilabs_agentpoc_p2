d9a5e16f57696f632a22bd13c74abc4f65bd585f
Atherusman <atherusman@mobilefirstapplication.com>
2023-11-07 15:47:22 +0530

    add graph

--- src/NewPages/AllocationEngine/ReviewPerformance.tsx
+++ src/NewPages/AllocationEngine/ReviewPerformance.tsx
@@ -4,107 +4,137 @@ import Performance from "./performance";
 import PLImpactChart from "./PLImpactChart";
 import SegmentTable from "./SegmentTable";
 import { useEffect, useState } from "react";
-import { HiPlus } from 'react-icons/hi'
+import { HiPlus } from "react-icons/hi";
 import TestId from "./TestId";
 import Treatment from "./Treatments";
 import SequenceAttributes from "./SequenceAttributes";
 
+const ReviewPerformance: React.FC = () => {
+  const [activeBucket, setActiveBucket] = useState("b1");
+  const [showTestIdComp, setShowTestIdComp] = useState(false);
+  const [showSequenceCond, setShowSequenceCond] = useState(false);
 
+  const BGroups = [
+    { id: "b1", label: "B1" },
+    { id: "b2", label: "B2" },
+    { id: "b3", label: "B3" },
+    { id: "b4", label: "B4" },
+    { id: "b5", label: "B5" },
+    { id: "b6", label: "B6" },
+  ];
 
+  const tableNumber = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30,
+  ];
 
+  const showSequence = () => {
+    setShowSequenceCond(true);
+  };
 
-const ReviewPerformance: React.FC = () =>{ 
+  const handleButtonClick = async (buttonId: string) => {
+    setActiveBucket(buttonId);
+  };
 
-   const [activeBucket, setActiveBucket] = useState("b1");
-   const [showTestIdComp,setShowTestIdComp] = useState(false)
-   const [showSequenceCond,setShowSequenceCond] = useState(false)
-
-
-   const BGroups = [
-      { id: "b1", label: "B1" },
-      { id: "b2", label: "B2" },
-      { id: "b3", label: "B3" },
-      { id: "b4", label: "B4" },
-      { id: "b5", label: "B5" },
-      { id: "b6", label: "B6" },
-    ];
-
-    const tableNumber = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-
-  const showSequence = () =>{
-   setShowSequenceCond(true)
-  }
-
-    const handleButtonClick = async (buttonId: string) => {
-      setActiveBucket(buttonId);
-    };
-
-    return(
-      <div className="flex flex-col gap-5 bg-gray-100 pl-2 pt-5 m-4 rounded-xl">
+  return (
+    <div className="flex flex-col gap-5 bg-gray-100 pl-2 pt-5 m-4 rounded-xl">
       <div className="w-[95%] flex items-center justify-between ml-6">
-      <div className=" flex justify-between  rounded-xl B1TabsContain">
-              {BGroups.map((button, index) => (
-                <div
-                  key={button.id}
-                  onClick={() => handleButtonClick(button.id)}
-                  className={`text-center text-[18px] font-medium font-['Calibri' !important] h-10 w-[78px] border border-gray-400 flex align-center justify-center items-center cursor-pointer ${
-                    activeBucket === button.id
-                      ? " bg-violet-200 "
-                      : "bg-[#fafafb]"
-                  } ${index === 0 ? "rounded-l-md" : ""} ${
-                    index === BGroups.length - 1 ? "rounded-r-md" : ""
-                  }`}
-                >
-                  {button.label}
-                </div>
-              ))}
+        <div className=" flex justify-between  rounded-xl B1TabsContain">
+          {BGroups.map((button, index) => (
+            <div
+              key={button.id}
+              onClick={() => handleButtonClick(button.id)}
+              className={`text-center text-[18px] font-medium font-['Calibri' !important] h-10 w-[78px] border border-gray-400 flex align-center justify-center items-center cursor-pointer ${
+                activeBucket === button.id ? " bg-violet-200 " : "bg-[#fafafb]"
+              } ${index === 0 ? "rounded-l-md" : ""} ${
+                index === BGroups.length - 1 ? "rounded-r-md" : ""
+              }`}
+            >
+              {button.label}
             </div>
+          ))}
+        </div>
 
-            <div className="flex items-center gap-1">
-                <button type="button" className="text-gray-500 border-2 pl-3 pr-3 rounded">HOLD</button>
-                <button type="button" className="text-white bg-green-500 border-2 pl-3 pr-3 rounded">MESSAGE</button>
-                <button type="button" className="text-white bg-violet-800 border-2 pl-3 pr-3 rounded">CALL</button>
-                <button type="button" className="text-white bg-orange-500 border-2 pl-3 pr-3 rounded">AGENCY</button>
-                <button type="button" className="text-white bg-red-600 border-2 pl-3 pr-3 rounded">LEGAL</button>
-
-            </div>
-
+        <div className="flex items-center gap-1">
+          <button
+            type="button"
+            className="text-gray-500 border-2 pl-3 pr-3 rounded"
+          >
+            HOLD
+          </button>
+          <button
+            type="button"
+            className="text-white bg-green-500 border-2 pl-3 pr-3 rounded"
+          >
+            MESSAGE
+          </button>
+          <button
+            type="button"
+            className="text-white bg-violet-800 border-2 pl-3 pr-3 rounded"
+          >
+            CALL
+          </button>
+          <button
+            type="button"
+            className="text-white bg-orange-500 border-2 pl-3 pr-3 rounded"
+          >
+            AGENCY
+          </button>
+          <button
+            type="button"
+            className="text-white bg-red-600 border-2 pl-3 pr-3 rounded"
+          >
+            LEGAL
+          </button>
+        </div>
       </div>
       <div className="flex flex-col">
-      <SegmentTable/>
-      <div className="w-[95%] flex items-center ml-6">
-         <div className="w-[20%] flex items-center gap-1 border-2 ">
-           <HiPlus className="text-violet-800" size={35}/>
-           <button onClick={()=>setShowTestIdComp(true)} type='button' className={`w-[90%] p-1 border-2 rounded-md font-['calibri' !important] font-[400] text-gray-500 ${showTestIdComp && 'bg-violet-300 text-gray-800 font-[500]' }`}>Add/Edit</button>
-         </div>
-         <table className="w-[80%]" cellPadding={5}>
-           <tbody>
-             <tr className="border-2">
-              {tableNumber.map(each=>(
-                <td className="border-2 text-center font-['calibri' !1important] font-[500s]">{each}</td>
-              ))}
-             </tr>
-             
-           </tbody>
-         </table>
-      </div>
-      </div>
-     {!showTestIdComp && <div className=" flex gap-2 ml-4">
-        <TestPipeLine/> 
-        <Performance/>
-        <PLImpactChart/>
-     </div>}
-     {showTestIdComp && <div className="flex gap-6 ml-5">
-      <div className="flex flex-col gap-2">
-       <TestId showSequence={showSequence}/>
-       {showSequenceCond && <SequenceAttributes/>}
-      </div>
-       <Treatment/>
+        <SegmentTable />
+        {/* <div className="w-[95%] flex items-center ml-6">
+          <div className="w-[20%] flex items-center gap-1 border-2 ">
+            <HiPlus className="text-violet-800" size={35} />
+            <button
+              onClick={() => setShowTestIdComp(true)}
+              type="button"
+              className={`w-[90%] p-1 border-2 rounded-md font-['calibri' !important] font-[400] text-gray-500 ${
+                showTestIdComp && "bg-violet-300 text-gray-800 font-[500]"
+              }`}
+            >
+              Add/Edit
+            </button>
+          </div>
 
-     </div>}
-     </div>
-    )
-    };
-    
-    export default ReviewPerformance;
-    
+          <table className="w-[80%]" cellPadding={5}>
+            <tbody>
+              <tr className="border-2">
+                {tableNumber.map((each) => (
+                  <td className="border-2 text-center font-['calibri' !1important] font-[500s]">
+                    {each}
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        </div> */}
+      </div>
+      {!showTestIdComp && (
+        <div className=" flex gap-2 ml-4">
+          <TestPipeLine />
+          <Performance />
+          <PLImpactChart />
+        </div>
+      )}
+      {showTestIdComp && (
+        <div className="flex gap-6 ml-5">
+          <div className="flex flex-col gap-2">
+            <TestId showSequence={showSequence} />
+            {showSequenceCond && <SequenceAttributes />}
+          </div>
+          <Treatment />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ReviewPerformance;
--- src/NewPages/AllocationEngine/allocationStackedBarChart.tsx
+++ src/NewPages/AllocationEngine/allocationStackedBarChart.tsx
@@ -1,57 +1,53 @@
-import React from 'react';
-import ReactApexChart from 'react-apexcharts';
-import {useEffect} from 'react'
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+import { useEffect } from "react";
 
 const AllocationStackedBarChart: React.FC = () => {
-
-    
-
   const state = {
     series: [
       {
-        name: 'Marine Sprite',
+        name: "Marine Sprite",
         data: [44, 55, 41, 37, 22],
       },
       {
-        name: 'Striking Calf',
+        name: "Striking Calf",
         data: [53, 32, 33, 52, 13],
       },
       {
-        name: 'Tank Picture',
+        name: "Tank Picture",
         data: [12, 17, 11, 9, 15],
       },
       {
-        name: 'Bucket Slope',
+        name: "Bucket Slope",
         data: [9, 7, 5, 8, 6],
       },
-    //   {
-    //     name: 'Reborn Kid',
-    //     data: [25, 12, 19, 32, 25],
-    //   },
+      //   {
+      //     name: 'Reborn Kid',
+      //     data: [25, 12, 19, 32, 25],
+      //   },
     ],
     options: {
       chart: {
-        type: 'bar',
+        type: "bar",
         height: 350,
         stacked: true,
-        stackType: '100%',
+        stackType: "100%",
       },
       plotOptions: {
         bar: {
           horizontal: true,
+          // distributed: true,
           dataLabels: {
             enabled: false, // Disable data labels on bars
           },
-          
-
         },
       },
       stroke: {
         width: 1,
-        colors: ['#fff'],
+        colors: ["#fff"],
       },
       title: {
-        text: '',
+        text: "",
       },
       xaxis: {
         categories: [2008, 2009, 2010, 2011, 2012],
@@ -59,10 +55,9 @@ const AllocationStackedBarChart: React.FC = () => {
       tooltip: {
         y: {
           formatter: (val: number) => {
-            return val + 'K';
+            return val + "K";
           },
         },
-        
       },
       fill: {
         opacity: 1,
@@ -73,36 +68,65 @@ const AllocationStackedBarChart: React.FC = () => {
       menu: {
         show: false,
       },
-      
     },
   };
 
-  (state.options.chart as any).type = 'bar';
+  (state.options.chart as any).type = "bar";
 
   useEffect(() => {
     // Hide the menu icon after the chart is rendered
-    const menuIcon = document.querySelector(".apexcharts-menu-icon") as HTMLElement;
+    const menuIcon = document.querySelector(
+      ".apexcharts-menu-icon"
+    ) as HTMLElement;
     if (menuIcon) {
       menuIcon.style.display = "none";
     }
   }, []);
 
-
   return (
     <div id="chart" className="w-[60%] bg-white border-2 p-3 rounded-xl">
-        <div className="flex justify-between items-center ml-3 mr-5">
-            <h1 className="text-[22px] font-[calibri] font-[500]">Treatments</h1>
-            <div className="flex items-center gap-1">
-                <button type="button" className="text-gray-500 border-2 pl-3 pr-3 rounded">HOLD</button>
-                <button type="button" className="text-white bg-green-500 border-2 pl-3 pr-3 rounded">MESSAGE</button>
-                <button type="button" className="text-white bg-violet-800 border-2 pl-3 pr-3 rounded">CALL</button>
-                <button type="button" className="text-white bg-orange-500 border-2 pl-3 pr-3 rounded">AGENCY</button>
-                <button type="button" className="text-white bg-red-600 border-2 pl-3 pr-3 rounded">LEGAL</button>
-
-            </div>
+      <div className="flex justify-between items-center ml-3 mr-5">
+        <h1 className="text-[22px] font-[calibri] font-[500]">Treatments</h1>
+        <div className="flex items-center gap-1">
+          <button
+            type="button"
+            className="text-gray-500 border-2 pl-3 pr-3 rounded"
+          >
+            HOLD
+          </button>
+          <button
+            type="button"
+            className="text-white bg-green-500 border-2 pl-3 pr-3 rounded"
+          >
+            MESSAGE
+          </button>
+          <button
+            type="button"
+            className="text-white bg-violet-800 border-2 pl-3 pr-3 rounded"
+          >
+            CALL
+          </button>
+          <button
+            type="button"
+            className="text-white bg-orange-500 border-2 pl-3 pr-3 rounded"
+          >
+            AGENCY
+          </button>
+          <button
+            type="button"
+            className="text-white bg-red-600 border-2 pl-3 pr-3 rounded"
+          >
+            LEGAL
+          </button>
         </div>
+      </div>
       {/* <ReactApexChart options={state.options} series={state.series} type="bar" height={350} /> */}
-      <ReactApexChart options={state.options as any} series={state.series} type="bar" height={350} />
+      <ReactApexChart
+        options={state.options as any}
+        series={state.series}
+        type="bar"
+        height={350}
+      />
     </div>
   );
 };
